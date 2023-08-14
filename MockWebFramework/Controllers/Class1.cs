6c7892d84a588f3882fb2ed5e0df240c9d764568
ioan <ioan.macavei2001@gmail.com>
2023-08-14 16:45:19 +0300

    Added controllers (testing with controlelrhost)

---
+++ MockWebFramework/Controllers/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MockWebFramework.Controller;
+using MockWebFramework.Controller.Attributes;
+
+namespace MockWebFramework.Controllers
+{
+    [ControllerPrefix("/")]
+    internal class Class1
+    {
+        [HttpGet("/a/id")]
+        public void RouteHandler1([FromRoute]int id)
+        {
+
+        }
+    }
+}
