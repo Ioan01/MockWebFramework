0877064bb2673df77af50a4721f6f0a9dfec848c
ioan <ioan.macavei2001@gmail.com>
2023-08-14 16:45:03 +0300

    Added servicehost skeleton

---
+++ MockWebFramework/Service/ServiceHost.cs
@@ -0,0 +1,18 @@
+﻿namespace MockWebFramework.Service
+{
+    internal class ServiceHost
+    {
+        private List<ServiceRegistration> _services = new();
+
+
+        public void AddSingleton(object service)
+        {
+
+        }
+
+        public void AddTransient(object service)
+        {
+
+        }
+    }
+}
---
+++ MockWebFramework/Service/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿namespace MockWebFramework.Service
+{
+    enum LifeSpan
+    {
+        Singleton,
+        Transient
+    }
+
+
+    internal class ServiceRegistration
+    {
+        public Type ServiceType { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public object Service { get; set; }
+
+
+    }
+}
