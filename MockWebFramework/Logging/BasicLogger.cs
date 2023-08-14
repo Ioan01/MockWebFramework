455fc9b501ace105776abcd998c34c899d5422bd
ioan <ioan.macavei2001@gmail.com>
2023-08-14 16:44:51 +0300

    Added basic logger

---
+++ MockWebFramework/Logging/BasicLogger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockWebFramework.Logging
+{
+    internal class BasicLogger : Ilogger
+    {
+        private MessageType _lasMessageType = MessageType.Info;
+
+        private string format(string type, string message) => $"{type} {DateTime.Now.ToString("HH:mm:ss")} {message}";
+
+        public void LogInfo(string message)
+        {
+            if (_lasMessageType != MessageType.Info)
+                Console.ResetColor();
+            Console.WriteLine(format("INFO",message));
+        }
+
+        public void LogWarning(string message)
+        {
+            if (_lasMessageType != MessageType.Warning)
+            {
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            Console.WriteLine(format("WARNING", message));
+        }
+
+        public void LogError(string message)
+        {
+            if (_lasMessageType != MessageType.Error)
+            {
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine(format("ERROR", message));
+        }
+
+        public void LogFatal(string message)
+        {
+            if (_lasMessageType != MessageType.Fatal)
+            {
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+            }
+
+            Console.WriteLine(format("FATAL", message));
+        }
+
+        public static BasicLogger Logger { get; private set; } = new BasicLogger();
+
+
+
+    }
+}
---
+++ MockWebFramework/Logging/Ilogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockWebFramework.Logging
+{
+
+    enum MessageType
+    {
+        Info,
+        Warning,
+        Error,
+        Fatal
+    }
+    internal interface Ilogger
+    {
+        void LogInfo(string message);
+
+        void LogWarning(string message);
+
+        void LogError(string message);
+
+        void LogFatal(string message);
+
+        public static Ilogger Instance => new BasicLogger();
+
+    }
+}
